1bca10e29b4d2b662daee5cf4b7c6a696de0dade
vaghasiyavikas <vikasvaghasiya1212@gmail.com>
2020-09-26 09:57:28 +0530

    -- change

--- src/templates/topic-page.js
+++ src/templates/topic-page.js
@@ -123,7 +123,6 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
         title
         featuredimage{
           childImageSharp {
