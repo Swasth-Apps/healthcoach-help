390fd3588e965f3e9c81faec3b72004c6bdc88ca
vaghasiyavikas <vikasvaghasiya1212@gmail.com>
2020-10-01 22:47:53 +0530

    -- Changes

--- src/templates/category-page.js
+++ src/templates/category-page.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/Layout'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import TopicsGrid from '../components/TopicsGrid';
-import Content, { HTMLContent } from '../components/Content'
-
-
+import Content, {HTMLContent} from '../components/Content'
 
 export const CategoryTemplate = ({
   feature,
